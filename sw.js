c064e65d1b87960ef5ba3831ded5ff00ad689b01
Averyscove <averyscove@gmail.com>
2026-05-06 15:11:01 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Bump CACHE_VERSION each time index.html or sw.js changes meaningfully.
  */
 
-const CACHE_VERSION = 'v4-2026-05-06';
+const CACHE_VERSION = 'v5-2026-05-06-auth';
 const CACHE_NAME = `bible-study-${CACHE_VERSION}`;
 const PRECACHE_URLS = ['./', './index.html', './sw.js'];
 
